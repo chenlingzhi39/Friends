92177252017b91a3ece2a4b5e19e26f21c53bfb0
chenlingzhi39 <chenlingzhi39@gmail.com>
2016-03-29 18:00:53 +0800

    fix

---
+++ daogenerator/src/main/java/com/example/Daogenerator.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+public class Daogenerator {
+
+    public static void main(String[] args) throws Exception {
+        int version = 1;
+        String defaultJavaPackage = "de.greenrobot.daoexample";
+        Schema schema = new Schema(version, defaultJavaPackage);
+        //addTable(schema);
+        addTable(schema);
+        // /表示根目录， ./表示当前路径， ../表示上一级父目录
+        new DaoGenerator().generateAll(schema, "./friends/src/main/java");
+    }
+private static void addSearch(Schema schema){
+    Entity item = schema.addEntity("Quick");
+    item.addIdProperty().notNull();
+    item.addStringProperty("content").notNull();
+}
+    private static void addTable(Schema schema) {
+        Entity note = schema.addEntity("NewsChannelTable");
+        //        note.addIdProperty();
+        /**
+         * 频道名称
+         */
+        note.addStringProperty("newsChannelName").notNull().primaryKey().index();
+        /**
+         * 频道id
+         */
+        note.addStringProperty("newsChannelId").notNull();
+        /**
+         * 频道类型
+         */
+        note.addStringProperty("newsChannelType").notNull();
+        /**
+         * 选中的频道
+         */
+        note.addBooleanProperty("newsChannelSelect").notNull();
+        /**
+         * 频道的排序位置
+         */
+        note.addIntProperty("newsChannelIndex").notNull();
+        /**
+         * 频道是否固定的
+         */
+        note.addBooleanProperty("newsChannelFixed");
+    }
+
+}
